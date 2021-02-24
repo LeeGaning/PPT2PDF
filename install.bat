a88114b35a6a6745a662a83638f64318ee96d4ee
Ganing <sht_jx@sina.com>
2021-02-24 10:45:53 +0800

    fixed:功能缺失

--- install.bat
+++ install.bat
@@ -1,2 +1,4 @@
-python3 -m virtualenv venv
- .\venv\Scripts\pip.exe install -i https://pypi.tuna.tsinghua.edu.cn/simple -r piplist.txt
+python3 -m venv --without-pip ./venv
+.\venv\Scripts\activate.bat
+.\venv\Scripts\python -m ensurepip
+.\venv\Scripts\pip.exe install -i https://pypi.tuna.tsinghua.edu.cn/simple -r piplist.txt
--- onebuild.bat
+++ onebuild.bat
@@ -1,3 +1,3 @@
-rmdir build /S /Q 
-rmdir dist /S /Q 
- .\venv\Scripts\pyinstaller --version-file file_version_info.txt -F PPT转换PDF.py -i pdf.ico
+@REM rmdir build /S /Q 
+@REM rmdir dist /S /Q 
+.\venv\Scripts\pyinstaller --version-file file_version_info.txt -F PPT2PDF.py -i pdf.ico
